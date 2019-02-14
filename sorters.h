359fd7c1f4e0170de1d885a06671f74062c1383e
Mrcer <2969944867@qq.com>
2019-02-14 10:02:50 +0800

    Add files via upload

---
+++ sorters.h
@@ -0,0 +1,20 @@
+/*
+ * sorters.h
+ *
+ *
+ *
+ */
+
+#ifndef SORTERS_H_
+#define SORTERS_H_
+
+//Include your sourse file here
+#include "example.cpp"
+#include "bubble_sort.cpp"
+#include "selection_sort.cpp"
+#include "merge_sort.cpp"
+#include "insertion_sort.cpp"
+#include "quick_sort.cpp"
+#include "heap_sort.cpp"
+#include "CountedInt.cpp"
+#endif /* SORTERS_H_ */
