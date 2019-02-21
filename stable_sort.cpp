048fc59a5f9a393c04c9abc819f3942d1dc7c15c
loveEnlin <47773868+loveEnlin@users.noreply.github.com>
2019-02-21 16:45:33 +0800

    Update stable_sort.cpp

--- stable_sort.cpp
+++ stable_sort.cpp
@@ -1,40 +1,58 @@
-#include<stdio.h>
-#define maxn 1000005
-int a[maxn],temp[maxn];
-void merge(int l,int r,int m){
-    int i = l, j = m + 1,k = l;
-    while(i<=m&&j<=r){
-        if(a[i] > a[j]){
-            temp[k++] = a[j++];
-        }else{
-            temp[k++] = a[i++];
-        }
-    }
-    while(i<=m)
-        temp[k++] = a[i++];
-    while(j<=r)
-        temp[k++] = a[j++];
-    for(i = l;i<=r;i++)
-        a[i] = temp[i];
+#include <main.cpp>
+using namespace std;
+void Merge(vector<int> &data, int &start, int &ending, vector<int> &res){
+  int is2 = start;
+  int ie2 = ending;
+  int left_index = is2;
+  int left_len = (ie2+is2+1)/2;
+  int right_index = is2+left_len;
+  int res_index = is2;
+  while((left_index < (is2+ left_len)) && (right_index < ie2+1)){
+    res[res_index++] = (data[left_index] < data[right_index]) ? data[left_index++] : data[right_index++];
+  }
+  while(left_index < (is2+ left_len)){
+    res[res_index++] = data[left_index++];
+  }
+  while(right_index <= ie2){
+    res[res_index++] = data[right_index++];
+  }
+//   for(int i =is2; i<ie2;++i){
+//     data[i] = res[i];
+//   }
 }
-void mergesort(int l,int r){
-    if(l<r){
-        int m = (l + r) / 2;
-        mergesort(l,m);
-        mergesort(m+1,r);
-        merge(l,r,m);
+ 
+void mergeSort(vector<int> &data, int &start, int &ending, vector<int> &res){
+  int is = start;
+  int ie = ending;
+  int mid = (is+ie+1)*0.5;
+  int midr = mid+1;
+  if(1 == ie-is){
+    if(data[is] > data[ie]){
+      Element::swap(data[is], data[ie]);
+      res[is] = data[is];
+      res[ie] = data[ie];
     }
+    return;
+  }
+  else if(0 == ie-is){
+    return;
+  }
+  else{
+    mergeSort(data, is, mid, res); // the left region
+    mergeSort(data, midr, ie, res); // the right region
+    
+    Merge(data, is, ie, res);
+//     for(int i = is;i<ie;++i){
+//       data[i] = res[i];
+//     } 
+  }
 }
+ 
 int main(){
-    int t;
-    scanf("%d",&t);
-    while(t--){
-        int n;
-        scanf("%d",&n);
-        for(int i = 0;i < n;i++){
-            scanf("%d",&a[i]);
-        }
-        mergesort(0,n-1);
-    }
-    return 0;
+  int length = data.size();
+  int len = data.size()-1;
+  vector<int> res;
+  res.resize(length);
+  mergeSort(data, start, len, res);
 }
+ 
